17362490bb73b15871a9c49662d561e2d613edfa
M10-Voxel <marvin.josch@gmail.com>
2026-06-21 13:56:16 +0200

    First Push

---
+++ Interactables/Animal/Animal.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+namespace AngryAnimals.Interactables.Animal;
+
+public partial class Animal : RigidBody2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
